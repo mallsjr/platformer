eade74c3b9c0f6b61ca01f6187c6e4a6c18f2b55
Michael Alls <allsjr@gmail.com>
2025-04-14 17:06:58 -0400

    player animations

--- main.lua
+++ main.lua
@@ -23,6 +23,9 @@ function love.load()
   player:setFixedRotation(true)
   player.speed = 240
   player.animation = animations.idle
+  player.isMoving = false
+  player.direction = 1
+  player.grounded = true
 
   platform = world:newRectangleCollider(250, 400, 300, 100, { collision_class = "Platform" })
   platform:setType("static")
@@ -36,16 +39,38 @@ function love.update(dt)
   world:update(dt)
 
   if player.body then
-    local px, py = player:getPosition()
+    local colliders = world:queryRectangleArea(player:getX() - 20, player:getY() + 50, 40, 2, { "Platform" })
+    if #colliders > 0 then
+      player.grounded = true
+    else
+      player.grounded = false
+    end
+
+    player.isMoving = false
+    local px, _ = player:getPosition() -- putting py to _ because not used
     if love.keyboard.isDown("right") then
       player:setX(px + player.speed * dt)
+      player.isMoving = true
+      player.direction = 1
     end
     if love.keyboard.isDown("left") then
       player:setX(px - player.speed * dt)
+      player.isMoving = true
+      player.direction = -1
     end
     if player:enter("Danger") then
       player:destroy()
     end
+  end
+
+  if player.grounded then
+    if player.isMoving then
+      player.animation = animations.run
+    else
+      player.animation = animations.idle
+    end
+  else
+    player.animation = animations.jump
   end
 
   player.animation:update(dt)
@@ -55,13 +80,12 @@ function love.draw()
   --draw
   world:draw() -- Don't want this enabled in actual game but helpful in debugging
   local px, py = player:getPosition()
-  player.animation:draw(sprites.playerSheet, px, py, nil, 0.25, nil, 130, 300)
+  player.animation:draw(sprites.playerSheet, px, py, nil, 0.25 * player.direction, 0.25, 130, 300)
 end
 
 function love.keypressed(key)
   if key == "up" or key == "space" then
-    local colliders = world:queryRectangleArea(player:getX() - 20, player:getY() + 50, 40, 2, { "Platform" })
-    if #colliders > 0 then
+    if player.grounded then
       player:applyLinearImpulse(0, -4000)
     end
   end
@@ -70,7 +94,7 @@ end
 function love.mousepressed(x, y, button)
   if button == 1 then
     local colliders = world:queryCircleArea(x, y, 200, { "Platform", "Danger" })
-    for i, c in ipairs(colliders) do
+    for _, c in ipairs(colliders) do
       c:destroy()
     end
   end
