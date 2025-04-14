89d15e2f98b6028001d5bc7124663135bdbf07eb
Michael Alls <allsjr@gmail.com>
2025-04-14 14:28:59 -0400

    player animations using anim8

--- main.lua
+++ main.lua
@@ -1,4 +1,16 @@
 function love.load()
+  anim8 = require("./libraries/anim8/anim8")
+
+  sprites = {}
+  sprites.playerSheet = love.graphics.newImage("sprites/playerSheet.png")
+
+  local grid = anim8.newGrid(614, 564, sprites.playerSheet:getWidth(), sprites.playerSheet:getHeight())
+
+  animations = {}
+  animations.idle = anim8.newAnimation(grid("1-15", 1), 0.05)
+  animations.jump = anim8.newAnimation(grid("1-7", 2), 0.05)
+  animations.run = anim8.newAnimation(grid("1-15", 3), 0.05)
+
   wf = require("./libraries/windfield/windfield/")
   world = wf.newWorld(0, 800, false)
   world:setQueryDebugDrawing(true)
@@ -10,6 +22,7 @@ function love.load()
   player = world:newRectangleCollider(360, 100, 80, 80, { collision_class = "Player" }) --physics object/collider
   player:setFixedRotation(true)
   player.speed = 240
+  player.animation = animations.run
 
   platform = world:newRectangleCollider(250, 400, 300, 100, { collision_class = "Platform" })
   platform:setType("static")
@@ -34,11 +47,14 @@ function love.update(dt)
       player:destroy()
     end
   end
+
+  player.animation:update(dt)
 end
 
 function love.draw()
   --draw
   world:draw() -- Don't want this enabled in actual game but helpful in debugging
+  player.animation:draw(sprites.playerSheet, 0, 0)
 end
 
 function love.keypressed(key)
